94ad6f5e954ac847b1b6bb78c4d4ea080c63913c
nathan <natebanion@gmail.com>
2020-06-28 16:19:52 -0400

    Improve documentation for src.data.fire_stations.

--- src/data/fire_stations.py
+++ src/data/fire_stations.py
@@ -1,12 +1,17 @@
 """Data processing for the fire stations data.
 
-Add details about:
+The processed fire station data may be useful for analyzing the locations and
+high-level attributes for fire stations across the US.
 
-- top level functions.
-- resulting data & metadata.
-- source data.
-- processing overview.
-- processing diagram.
+This module includes several top-level functions:
+
+- :func:`src.data.fire_stations.process` processes raw fire station data.
+- :func:`src.data.fire_stations.read` reads processed fire station data.
+
+Run this module as a script to process fire station data from raw inputs and
+save the processed data to the default data directory. ::
+
+  $ python -m src.data.fire_stations
 
 """
 import geopandas
@@ -22,19 +27,29 @@ PATH = utils.DATA["processed"] / "fire-stations.csv"
 
 def process():
     """Process the raw fire stations data.
-    
+
+    This function reads raw fire station data and appends column *GEOID10*,
+    which identifies the 2010 Census tract that each station belongs to. The
+    calculation occurs in three steps.
+
+    1. Identify fire station locations from latitude and longitude.
+    2. Identify 2010 Census tract boundaries from raw shape files.
+    3. Perform a spatial left-join, matching stations to surrounding tracts.
+
+    The resulting pandas.DataFrame has the same row count as the raw data.
+
     Returns:
         pandas.DataFrame: Processed fire station data.
     """
     stations = raw.read_fire_stations()
     tracts = raw.read_shapefiles()[["GEOID10", "geometry"]]
     
-    # Create points for each lon-lat pair.
+    # Create points for the lon-lat pair at each fire station.
     geometry = [] 
     for coord in zip(stations.Longitude, stations.Latitude):
         geometry.append(shapely.geometry.Point(coord))
     
-    # Add points to convert fd to a geodataframe.
+    # Append the points to the fire station data.
     stations = geopandas.GeoDataFrame(stations,
                                       crs="EPSG:4269",
                                       geometry=geometry)
@@ -47,6 +62,10 @@ def process():
 
 def read(process_if_needed=False):
     """Read processed fire stations data.
+
+    If `process_if_needed` is True and the processed fire station data are not
+    found on disk, then this function will process the fire stations data from
+    raw data and return the result.
 
     Args:
         process_if_needed (bool): Process the data if data not found.
