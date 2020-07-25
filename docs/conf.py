319ad5a69ea877901b564a9438782118d91dced1
nathan <natebanion@gmail.com>
2020-07-25 13:01:34 -0400

    Change the name of the project to "rcp2" in the docs.

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'RCP-2'
+project = u'rcp2'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
