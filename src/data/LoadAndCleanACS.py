78e2cfa94f4fe02094b561590ec66efec71b5abf
kelsonSS <K.shillingscrivo@gmail.com>
2020-12-08 20:07:24 -0500

    ACS Munging
    Adding Munging for:
    Education
    housing Price
    housing Age

--- src/data/LoadAndCleanACS.py
+++ src/data/LoadAndCleanACS.py
@@ -14,8 +14,8 @@ def LoadACS():
 
 
 def CleanACS(ACS):
-    # ACS Munging
     
+    ## Cleaning 
     # Ensures GEOID variable is in the correct format and sets it as the dataframe index
     ACS['GEOID'] = ACS['GEOID'].str[2:]
     
@@ -48,6 +48,27 @@ def CleanACS(ACS):
     # Remove missing values from dataframe
     ACS.replace([np.inf, -np.inf], np.nan,inplace = True)
     ACS.dropna(inplace = True)
+    
+    ## ACS Munging
+    #education adjustment 
+    ACS['educ_less_12th'] =  ACS.loc[:,'educ_nursery_4th':'educ_12th_no_diploma'].sum(axis =1 )
+    ACS['educ_high_school'] =  ACS.loc[:,'educ_high_school_grad':'educ_some_col_no_grad'].sum(axis =1 )
+    ACS.drop(ACS.loc[:, 'educ_nursery_4th':'educ_some_col_no_grad'], inplace = True, axis = 1)
+
+    # house age adjustment 
+    ACS['house_yr_pct_before_1960'] =ACS.loc[:,'house_yr_pct_1960_1969':'house_yr_pct_earlier_1939'].sum(axis =1 )
+    ACS.drop(ACS.loc[:, 'house_yr_pct_1960_1969':'house_yr_pct_earlier_1939'], inplace = True, axis = 1)
+    
+    # housing Price adjustment
+    ACS['house_val_less_50K']=ACS.loc[:,'house_val_less_10K':'house_val_40K_50K'].sum(axis =1 )
+    ACS['house_val_50_100K']=ACS.loc[:,'house_val_50K_60K':'house_val_90K_100K'].sum(axis =1 )
+    ACS['house_val_100K_300K']=ACS.loc[:,'house_val_100K_125K':'house_val_250K_300K'].sum(axis =1 )
+    ACS['house_val_300K_500K']=ACS.loc[:,'house_val_300K_400K':'house_val_400K_500K'].sum(axis =1 )
+    ACS['house_val_more_500k'] = ACS.loc[:,'house_val_500K_750K':'house_val_more_2M'].sum(axis = 1)
+    ACS.drop(ACS.loc[:, 'house_val_less_10K':'house_val_more_2M'], inplace = True, axis = 1)
+
+    
+    
     return ACS
 
 
